5b4ef575429ff09f3d6cc573be34d6dfdab049a4
HerDataDome <trishachatterjeeofficial@gmail.com>
2026-02-25 18:54:06 +0100

    feat: add DTOs for Booking and Auth with Lombok Builder

---
+++ src/test/java/com/booking/dto/Booking.java
@@ -0,0 +1,22 @@
+package com.booking.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Booking {
+    private Object roomid;
+    private String firstname;
+    private String lastname;
+    private Object depositpaid;
+    private BookingDates bookingdates;
+    private String email;
+    private String phone;
+}
---
+++ src/test/java/com/booking/dto/TokenRequest.java
@@ -0,0 +1,15 @@
+package com.booking.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenRequest {
+    private String username;
+    private String password;
+}
